7c2673e6ccaa266bc16325d6a8feda1cf6348824
Terry Li <lhddragon87@gmail.com>
2017-09-28 23:16:45 -0400

    Update main.js

--- src/content/getusermedia/gum/js/main.js
+++ src/content/getusermedia/gum/js/main.js
@@ -14,7 +14,9 @@ var video = document.querySelector('video');
 // Put variables in global scope to make them available to the browser console.
 var constraints = window.constraints = {
   audio: false,
-  video: true
+  video: {
+      facingMode: { exact: "environment" }
+  }
 };
 
 function handleSuccess(stream) {
